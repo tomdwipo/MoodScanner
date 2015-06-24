c6c338d48d4f7d265ea9b90f595cccf3d77ad6a3
tommyputranto <tommyputranto1@gmail.com>
2015-06-24 20:22:28 +0700

    coool

--- app/src/main/java/com/example/android/moodscanner/MainActivity.java
+++ app/src/main/java/com/example/android/moodscanner/MainActivity.java
@@ -1,16 +1,71 @@
 package com.example.android.moodscanner;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView thumbPrint;
+    private TextView result;
+    private AnimationDrawable thumbAnimation;
+    private String[] moodResult;
+    private Runnable mRunnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        moodResult = new String[] {
+                "Someone is cranky!",
+                "You are my sunshine!",
+                "No comments...",
+                "You're stressed out!",
+                "Happy camper :)",
+                "Not your day :(",
+                "Smile - it's good for you.",
+                "In the clouds...",
+                "Pensive!",
+                "Sad!",
+                "Excited!"
+
+        };
+        thumbPrint = (ImageView)findViewById(R.id.thumbPrint);
+        thumbPrint.setBackgroundResource(R.drawable.thumb_animation);
+        thumbAnimation = (AnimationDrawable)thumbPrint.getBackground();
+        result = (TextView)findViewById(R.id.textResult);
+
+        thumbPrint.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+              //  Toast.makeText(getApplicationContext(),"lama",Toast.LENGTH_LONG).show();
+                thumbAnimation.start();
+                showResult();
+
+
+                return false;
+            }
+        });
+
+    }
+
+    public void showResult(){
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+             int rand = (int) (Math.random() * moodResult.length);
+                result.setText(moodResult[rand]);
+                thumbAnimation.stop();
+            }
+        };
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(mRunnable, 4000);
+
     }
 
     @Override
